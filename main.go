07a40e05c08a62ce7963b252750970a6e0d26237
Luo Hao <luohao.brian@gmail.com>
2018-06-14 10:53:17 +0800

    改名key名称从dingo到SimplePosts

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	portPtr := flag.String("port", "8000", "The port number for Dingo to listen to.")
-	privKeyPathPtr := flag.String("priv-key", "dingo.rsa", "The private key file path for JWT.")
-	pubKeyPathPtr := flag.String("pub-key", "dingo.rsa.pub", "The public key file path for JWT.")
+	privKeyPathPtr := flag.String("priv-key", "SimplePosts.rsa", "The private key file path for JWT.")
+	pubKeyPathPtr := flag.String("pub-key", "SimplePosts.rsa.pub", "The public key file path for JWT.")
 	flag.Parse()
 	//Dingo.Init()
 	Dingo.Init(*privKeyPathPtr, *pubKeyPathPtr)
